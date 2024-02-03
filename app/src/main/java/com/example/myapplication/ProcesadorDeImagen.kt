03764f69c547a94a4900bf689bcf5d61702133d8
Pablo Jose <pbl808hs@gmail.com>
2024-02-03 17:17:20 -0600

    initial commit

---
+++ app/src/main/java/com/example/myapplication/ProcesadorDeImagen.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import android.graphics.Bitmap
+
+class ProcesadorDeImagen {
+    fun convolution(image: Bitmap): Bitmap {
+        TODO()
+    }
+    fun edgeDetection(image: Bitmap): Bitmap {
+        TODO()
+    }
+    fun laplacian(image: Bitmap): Bitmap {
+        TODO()
+    }
+    fun gaussianFilter(image: Bitmap): Bitmap {
+        TODO()
+    }
+}
